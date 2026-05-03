95eca9d2000401b0fffabf5a5394387f5d40eafe
Zakaria Hamdaoui <info@zaka.cf>
2026-05-03 23:46:36 +0200

    Merge main i fix Scene.cpp

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1680,11 +1680,7 @@ void Scene::InitFragments(int winW, int winH, int childX, int childW)
 	float faceTop = 0.0f;
 	float faceBottom = (float)winH * 0.45f;
 	float halfW = (float)winW * 0.5f;
-<<<<<<< HEAD
-	float halfH = (float)winH * 0.5f;  
-=======
 	float halfH = (float)winH * 0.5f;
->>>>>>> main
 
 	for (int i = 0; i < NUM_FRAGMENTS; i++) {
 		auto& f = fragments_[i];
@@ -1693,39 +1689,18 @@ void Scene::InitFragments(int winW, int winH, int childX, int childW)
 		float tw = 0, th = 0;
 		SDL_GetTextureSize(f.tex, &tw, &th);
 
-<<<<<<< HEAD
-		float sc = RandF(0.30f, 0.42f);
-=======
 		float sc = RandF(0.25f, 0.30f);
->>>>>>> main
 		f.w = (float)winW * sc;
 		f.h = f.w * (th / tw);
 
 		f.inFront = (i < 3);
 
-<<<<<<< HEAD
-		// Logical distribution to AVOID the face (upper center-right part of the illustration)
-		// We push them towards the edges of the right half or the bottom
-		if (i % 2 == 0) {
-			// Prefer bottom area
-			f.x = RandF(halfW - 50.0f, (float)winW - f.w * 0.5f);
-			f.y = RandF(halfH, (float)winH - f.h - 10.0f);
-		}
-		else {
-			// Prefer side areas (far right or closer to center but not top-center)
-			if (i == 1) f.x = RandF(halfW - 30.0f, halfW + 100.0f);
-			else        f.x = RandF((float)winW - f.w - 20.0f, (float)winW - 10.0f);
-			
-			f.y = RandF(10.0f, halfH);
-		}
-=======
 		float padX = 10.0f, padY = 15.0f;
 		if (i == 0) { f.x = halfW + padX;                                  f.y = (float)winH - f.h - padY; }
 		else if (i == 1) { f.x = halfW + ((float)winW - halfW) / 2.0f - (f.w / 2.0f); f.y = (float)winH - f.h - padY; }
 		else if (i == 2) { f.x = (float)winW - f.w - padX;                             f.y = (float)winH - f.h - padY; }
 		else if (i == 3) { f.x = halfW + padX;                                  f.y = padY + 10.0f; }
 		else if (i == 4) { f.x = (float)winW - f.w - padX;                             f.y = padY + 10.0f; }
->>>>>>> main
 
 		f.floatSpeed = RandF(0.4f, 0.9f);
 		f.floatAmplitude = RandF(8.0f, 22.0f);
