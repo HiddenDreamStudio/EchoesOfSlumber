034e9a24f075679a42bed0f710dac20d488e16b3
Bernif0x <bernatloza@gmail.com>
2026-05-20 23:46:40 +0200

    assets correctly saved

--- assets/maps/Assets level 4/foreground/fore level 4.tsx
+++ assets/maps/Assets level 4/foreground/fore level 4.tsx
@@ -1,2 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="fore level 4" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.0" name="fore level 4" tilewidth="5917" tileheight="1897" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Asset 9.png" width="3122" height="1420"/>
+ </tile>
+ <tile id="1">
+  <image source="Asset 10.png" width="977" height="1873"/>
+ </tile>
+ <tile id="2">
+  <image source="Asset 11.png" width="1822" height="1792"/>
+ </tile>
+ <tile id="3">
+  <image source="Asset 12.png" width="2560" height="1741"/>
+ </tile>
+ <tile id="4">
+  <image source="Asset 13.png" width="5917" height="1427"/>
+ </tile>
+ <tile id="5">
+  <image source="Asset 14.png" width="2950" height="1503"/>
+ </tile>
+ <tile id="6">
+  <image source="Asset 15.png" width="1513" height="1851"/>
+ </tile>
+ <tile id="7">
+  <image source="Asset 1.png" width="1392" height="1814"/>
+ </tile>
+ <tile id="8">
+  <image source="Asset 2.png" width="1895" height="1657"/>
+ </tile>
+ <tile id="9">
+  <image source="Asset 3.png" width="1541" height="1813"/>
+ </tile>
+ <tile id="10">
+  <image source="Asset 4.png" width="3660" height="1563"/>
+ </tile>
+ <tile id="11">
+  <image source="Asset 5.png" width="5081" height="1691"/>
+ </tile>
+ <tile id="12">
+  <image source="Asset 6.png" width="2507" height="1667"/>
+ </tile>
+ <tile id="13">
+  <image source="Asset 7.png" width="2900" height="1682"/>
+ </tile>
+ <tile id="14">
+  <image source="Asset 8.png" width="1417" height="1897"/>
+ </tile>
+</tileset>
